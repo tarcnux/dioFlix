685efacd5f2af7aabc9780953fec9457f0c91632
Tarcisio Nunes <github@tarcnux.com.br>
2020-09-23 23:56:19 -0300

    Estilizar owlCarousel

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 console.log("Netflix Front Clone - Digital Innovation One");
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
